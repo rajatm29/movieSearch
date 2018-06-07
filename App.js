e5b0e5ff0c88b159a3a9d86bcfe16847ccfaaaf3
Rajat Monga <rajatmonga29@gmail.com>
2018-06-06 23:57:16 -0500

    Update App.js

--- App.js
+++ App.js
@@ -12,15 +12,12 @@ class App extends Component {
         const movies = [
             {id: 0, poster_src: "https://images-na.ssl-images-amazon.com/images/I/81EzudL0k1L._SX342_.jpg",title: "Dhooom 3", overview: "bollywood movie thats pretty good"},
             {id: 1, poster_src: "https://upload.wikimedia.org/wikipedia/en/thumb/a/a3/D2-poster-ver2.jpg/220px-D2-poster-ver2.jpg", title: "Dhooom Avengers Edition", overview: "bollywood movie thats also pretty good"}
-
         ];
 
         var movieRows = [];
 
         movies.forEach((movie) => {
             console.log(movie.title);
-
-
             const movieRow = <table key={movie.id}>
                 <tbody>
                     <tr>
@@ -36,7 +33,6 @@ class App extends Component {
             </table>
             movieRows.push(movieRow)
         });
-
         this.state = {rows: movieRows};
     }
 
@@ -44,10 +40,8 @@ class App extends Component {
     render() {
         return (
             <div>
-
                 <table className="titleBar">
                     <tbody>
-
                         <tr>
                             <td>
                                 <img src={logo} className="App-logo" alt="logo" width='50' />
@@ -71,8 +65,6 @@ class App extends Component {
                 }} placeholder='Enter search term'/>
 
                 {this.state.rows}
-
-
             </div>
         );
     }
