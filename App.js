a25761c11699473a65f93a3feb066f552a07ed9e
Rajat Monga <rajatmonga@Rajats-MacBook-Pro.local>
2018-06-06 23:56:01 -0500

    added movie info

--- App.js
+++ App.js
@@ -3,9 +3,48 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+    constructor(props) {
+
+        super(props);
+        console.log('initializer');
+
+
+        const movies = [
+            {id: 0, poster_src: "https://images-na.ssl-images-amazon.com/images/I/81EzudL0k1L._SX342_.jpg",title: "Dhooom 3", overview: "bollywood movie thats pretty good"},
+            {id: 1, poster_src: "https://upload.wikimedia.org/wikipedia/en/thumb/a/a3/D2-poster-ver2.jpg/220px-D2-poster-ver2.jpg", title: "Dhooom Avengers Edition", overview: "bollywood movie thats also pretty good"}
+
+        ];
+
+        var movieRows = [];
+
+        movies.forEach((movie) => {
+            console.log(movie.title);
+
+
+            const movieRow = <table key={movie.id}>
+                <tbody>
+                    <tr>
+                        <td>
+                            <img src={movie.poster_src} width="150" alt="poster"/>
+                        </td>
+                        <td>
+                            {movie.title}
+                            <p>{movie.overview}</p>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            movieRows.push(movieRow)
+        });
+
+        this.state = {rows: movieRows};
+    }
+
+
     render() {
         return (
-            <div className="App" >
+            <div>
 
                 <table className="titleBar">
                     <tbody>
@@ -23,7 +62,16 @@ class App extends Component {
                     </tbody>
                 </table>
 
-                <input placeholder='Enter search term'></input>
+                <input style={{
+                    fontSize: 24,
+                    display: 'block',
+                    width:'99%',
+                    paddingLeft: 16,
+                    paddingTop: 8,
+                    paddingBottom: 8
+                }} placeholder='Enter search term'/>
+
+                {this.state.rows}
 
 
             </div>
@@ -32,4 +80,3 @@ class App extends Component {
 }
 
 export default App;
-
